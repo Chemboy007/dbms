20a2d5fd1139de99778c86205cf0145c9da99463
Chemboy007 <2018kucp1076@iiitkota.ac.in>
2019-11-03 10:29:21 +0530

    server

---
+++ sever.js
@@ -0,0 +1,65 @@
+var mysql = require('mysql')
+var express = require('express')
+var app = express()
+var path = require('path')
+
+app.listen(8080, function(err){
+    if (err) throw err;
+});
+
+var con = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "goodgame",
+    database : "college_mag"
+})
+
+app.set('views',  path.join(__dirname + '/views'))
+app.set('view engine', 'ejs')
+app.use( express.static( "assets" ) );
+
+app.get('/',function(req, res){
+    res.render('home')
+})
+
+// app.get('/student_login',function(req, res){
+    
+//     res.sendFile()
+// })
+
+// app.post('/student_check', function(req, res){
+//     sql = ``
+//     con.query(sql, function(err, result, fields){
+
+//     })
+// })
+
+
+
+// app.get('/admin_login', function(req, res){
+
+// })
+
+// app.post('/student_check', function(req, res){
+//     sql = `select pwd from student where username = ${req.body.username}`
+//     con.query(sql, function(err, result, fields){
+
+
+//     })
+// })
+
+
+// app.get('/registration', function(req, res){
+//     sql = ``
+// })
+
+// app.post('/enter_data', function(req, res){
+//     sql = `insert into student values (${req.body.username}, ${req.body.pwd}, ${req.body.contact}, ${req.body.email}, ${req.body.address}, ${req.body.dob});`
+//     con.query(sql, function(err){
+        
+//     })
+// })
+
+// app.get('/artical_upload', function(req, res){
+
+// })

3443933e5154b909f063ee3f4b12eb33ac1f886c
Chemboy007 <2018kucp1076@iiitkota.ac.in>
2019-11-03 16:22:11 +0530

    sever

--- sever.js
+++ sever.js
@@ -2,6 +2,7 @@ var mysql = require('mysql')
 var express = require('express')
 var app = express()
 var path = require('path')
+var student = "Guest"
 
 app.listen(8080, function(err){
     if (err) throw err;
@@ -19,51 +20,83 @@ app.set('view engine', 'ejs')
 app.use( express.static( "assets" ) );
 
 app.get('/',function(req, res){
-    res.render('home')
+    res.render('home', {name : student})
     res.end()
 })
-
 app.get('/home',function(req, res){
-    res.render('home')
+    res.redirect('/')
 })
-
-// app.get('/student_login',function(req, res){
-    
-//     res.sendFile()
-// })
-
-// app.post('/student_check', function(req, res){
-//     sql = ``
-//     con.query(sql, function(err, result, fields){
-
-//     })
-// })
-
 
 
 app.get('/admin', function(req, res){
     res.render('admin',{data: 'Admin Login'})
 })
 
-// app.post('/student_check', function(req, res){
-//     sql = `select pwd from student where username = ${req.body.username}`
-//     con.query(sql, function(err, result, fields){
+app.post('/admin_check',function(req, res){
+    var sql = `select pwd from admin where username = ${req.body.username};`
+    con.query(sql, function(err, result, fields){
+        // if (result.pwd == result.username){
+        //     res.redirect('/admin')
+        // }
+        if (result.pwd == req.body.pwd){
+            student = req.body.username
+            res.redirect('/')
+        }
+        else{
+            res.redirect('/admin')
+        }
+    })
+})
+
+app.get('/student',function(req, res){
+    res.render('student')
+})
+
+app.post('/student_check', function(req, res){
+    sql = `select pwd from student where username = ${req.body.username};`
+    con.query(sql, function(err, result, fields){
+        if (result.pwd == req.body.pwd){
+            student = req.body.username
+            res.redirect('/')
+        }
+        else{
+            res.redirect('/student_login')
+        }
+    })
+})
 
 
-//     })
-// })
+app.get('/upload',function(req, res){
+    if(student == "Guest"){
+        res.redirect('student_login')
+    }
+    else{
+        res.render('upload',{username : student})
+    }
+})
 
 
-// app.get('/registration', function(req, res){
-//     sql = ``
-// })
+app.get('/registration', function(req, res){
+    res.render('registration')
 
-// app.post('/enter_data', function(req, res){
-//     sql = `insert into student values (${req.body.username}, ${req.body.pwd}, ${req.body.contact}, ${req.body.email}, ${req.body.address}, ${req.body.dob});`
-//     con.query(sql, function(err){
-        
-//     })
-// })
+})
+
+app.post('/enter_data', function(req, res){
+    sql = `insert into student values (${req.body.username}, ${req.body.pwd}, ${req.body.contact}, ${req.body.email}, ${req.body.address}, ${req.body.dob});`
+    con.query(sql, function(err){
+        if(err){
+            throw err;
+            res.redirect('/registration')
+        } 
+
+    })
+})
+
+
+
+
+
+
 
 // app.get('/artical_upload', function(req, res){
 

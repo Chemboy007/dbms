9f2138f8bb3dcdb95699a96849a34d043948d334
Chemboy007 <2018kucp1076@iiitkota.ac.in>
2019-11-03 22:34:39 +0530

    sever

--- sever.js
+++ sever.js
@@ -3,56 +3,71 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var student = "Guest"
+var bodyParser = require('body-parser')
 
-app.listen(8080, function(err){
+
+app.listen(8080, function (err) {
     if (err) throw err;
+    console.log("listening on port" + 8080)
 });
+app.use(express.urlencoded({ extended: true }));
 
 var con = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "goodgame",
-    database : "college_mag"
+    host: "localhost",
+    user: "root",
+    password: "goodgame",
+    database: "college_mag"
 })
+con.connect((error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("connected to database");
 
-app.set('views',  path.join(__dirname + '/views'))
+})
+app.set('views', path.join(__dirname + '/views'))
 app.set('view engine', 'ejs')
-app.use( express.static( "assets" ) );
+app.use(express.static("assets"));
+app.use(bodyParser.json())
 
-app.get('/',function(req, res){
-    res.render('home', {name : student})
+app.get('/', function (req, res) {
+    res.render('home', { name: student })
     res.end()
 })
-app.get('/home',function(req, res){
+app.get('/home', function (req, res) {
     res.redirect('/')
 })
 
 
-app.get('/admin', function(req, res){
-    res.render('admin',{data: 'Admin Login'})
+app.get('/admin', function (req, res) {
+    res.render('admin', { data: 'Admin Login' })
 })
 
-app.post('/admin_check',function(req, res){
-    var sql = `select pwd from admin where username = ${req.body.username};`
-    con.query(sql, function(err, result, fields){
-        // if (result.pwd == result.username){
-        //     res.redirect('/admin')
-        // }
-        if (result.pwd == req.body.pwd){
-            student = req.body.username
-            res.redirect('/')
-        }
-        else{
+app.post('/admin_check', function (req, res) {
+    console.log(req.body);
+
+    // var sql = `select pwd from admin where username = ${req.body.Username}`
+    con.query("select pwd from admin where username = ?",[req.body.Username], function(err, result, fields){
+        console.log(result.length)
+        if (err) throw err;
+        if (result.length == 0){
             res.redirect('/admin')
         }
+        else{
+            student = req.body.Username
+            res.redirect('/')
+        }
+
+    
     })
 })
 
-app.get('/student',function(req, res){
-    res.render('student')
+app.get('/student', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/studen.html'))
 })
 
-app.post('/student_check', function(req, res){
+app.post('/student_check', function (req, res) {
+    res.send(`${req.body.hello}`)
     sql = `select pwd from student where username = ${req.body.username};`
     con.query(sql, function(err, result, fields){
         if (result.pwd == req.body.pwd){
@@ -66,36 +81,29 @@ app.post('/student_check', function(req, res){
 })
 
 
-app.get('/upload',function(req, res){
+app.get('/upload', function (req, res) {
     if(student == "Guest"){
         res.redirect('student_login')
     }
     else{
-        res.render('upload',{username : student})
+    res.render('upload', { username: student })
     }
 })
 
 
-app.get('/registration', function(req, res){
-    res.render('registration')
-
+app.get('/stureg', function (req, res) {
+    res.render('studentreg')
 })
 
-app.post('/enter_data', function(req, res){
+app.post('/enter_data', function (req, res) {
     sql = `insert into student values (${req.body.username}, ${req.body.pwd}, ${req.body.contact}, ${req.body.email}, ${req.body.address}, ${req.body.dob});`
-    con.query(sql, function(err){
-        if(err){
+    con.query(sql, function (err) {
+        if (err) {
             throw err;
             res.redirect('/registration')
-        } 
-
+        }
     })
 })
-
-
-
-
-
 
 
 // app.get('/artical_upload', function(req, res){
